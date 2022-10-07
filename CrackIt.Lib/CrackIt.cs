fc1863552a14a0d7618de440bd3edfb099d59d88
impscorp <jonvoegel@tsn.at>
2022-10-07 22:58:30 +0200

    Initial commit

---
+++ CrackIt.Lib/CrackIt.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CrackIt.Lib
+{
+    public class CrackIt
+    {
+        public static string GeneratePassword(int length)
+        {
+            const string valid = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+    }
+}
---
+++ Crackit/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] passwords = new string[100000];
+        for (int j = 0; j < passwords.Length; j++)
+        {
+            //start code in 20 threads
+
+            Task[] tasks = new Task[20];
+            for (int i = 0; i < 20; i++)
+            {
+                //random number between 3 and 5 digits
+                int num = new Random().Next(3, 5);
+                tasks[i] = Task.Run(() =>
+                {
+                    //go through every possibility of Generate Password
+                    Console.WriteLine(CrackIt.Lib.CrackIt.GeneratePassword(num));
+                    //passwords.Append(CrackIt.Lib.CrackIt.GeneratePassword(num));
+                });
+            }
+            Task.WaitAll(tasks);
+        }
+    }
+}
+
+
+
